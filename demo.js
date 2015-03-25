afff8c7d0a63e565aa7370fab4c4c61738bd7bb2
Wpdas <wpdas@yahoo.com.br>
2015-03-25 18:28:12 -0300

    MobileConsole (mConsole) v1.0.0
    First release mConsole.

---
+++ demo.js
@@ -0,0 +1,22 @@
+/*!
+ * mConsole Demo (mConsole) v1.0.0
+ * Author: Wenderson Pires | @wpdas
+ * Copyright (c) 2015
+ * Licensed under MIT
+ */
+
+window.onload = function(){
+
+    //Start mobile console and pass height (optional) per parameter.
+    //$mConsole.init(250);
+    $mConsole.init();
+
+    //Execute console
+    console.log("My log");
+    console.warn("My warning");
+    console.info("My info");
+    console.debug("My debug");
+
+    //Proposital error
+    myErrorVar;
+};
